4100735a56f4cf3f39b7dfca4c17be98a4f91f69
pardeshiaryan <pardeshiaryanon@gmail.com>
2026-06-30 11:14:52 +0530

    adding google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { socialMedia } from '@/data'
 import Link from 'next/link'
 import { TracingBeam } from '@/components/ui/tracing-beam'
 import { ThemeProvider } from './provider'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-N6TR59DSTW"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+function gtag(){window.dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-N6TR59DSTW');`}
+        </Script>
+      </head>
+
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
